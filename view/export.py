c6b8b7c37c57068624d19445d3d4132de25d9772
David Santos <davidsantos2807@gmail.com>
2023-06-30 09:51:49 +0100

    Export as JSON

--- view/export.py
+++ view/export.py
@@ -7,6 +7,8 @@ File imported by mainView.py. Renders all of the widgets belonging to the export
 """
 import customtkinter as ctk
 import json
+from customtkinter import filedialog as fd
+from os import getcwdb
 
 class export(ctk.CTkToplevel):
 
@@ -21,17 +23,40 @@ class export(ctk.CTkToplevel):
         selectFormat.grid(row=0,column=1,padx=10)
         btnExport = ctk.CTkButton(self,text="Export",command=self.exportData)
         btnExport.grid(row=1,columnspan=2,padx=10,pady=20)
+        self.labelMessage = ctk.CTkLabel(self,text="")
+        self.labelMessage.grid(row=2,columnspan=2,padx=10,pady=10)
 
     def exportData(self):
         format = self.format.get()
-        data = {"date": "day/month/year"}
+        """Export JSON format
+        data = {"dates": [
+            {
+                "day":"dd",
+                "month": "mm",
+                "year": "yyyy",
+                "foods": [
+                    {
+                        "name": "food example",
+                        "calories": value
+                    }
+                ]
+            }
+            ]}"""
+        
+        data= {"dates": []}
         query_dates = self.master.db.getDates()
         for date in query_dates:
-            data.update({"date": date})
+            foods_list = self.master.db.getFoodsByDateId(date[1],date[2],date[3])
+            data["dates"].append({"day":date[1],"month":date[2],"year":date[3],"foods":foods_list})
 
         if format == "JSON":
-            json_data = json.dumps(list(data))
-            with open("calorieLoggerExport.json","w") as outfile:
-                outfile.write(json_data)
+            json_data = json.dumps(data,indent=4)
+            file_types = (('JSON files', '*.json'),('All files', '*.*'))
+            try:
+                filename = fd.asksaveasfilename(title="Choose export location",filetypes=file_types,initialdir=getcwdb())
+                with open(filename,"w") as outfile:
+                    outfile.write(json_data)
+                self.labelMessage.configure(text=f"Data exported!\n{filename}")
+            except: pass
         elif format == "CSV":
             print(format)
